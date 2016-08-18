3194eb83bfa87a7545d2d717bd402428bcc6e336
Chris Wintle <cwintle@andigital.com>
2016-08-18 10:39:39 +0100

    add terraform plan to codebase

---
+++ terraform-prod.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region = "eu-west-1"
+}
+
+resource "aws_instance" "instance_1" {
+    ami = "ami-0451d977"
+    instance_type = "t2.micro"
+    security_groups = ["chris-sec-group"]
+
+    tags {
+        Name = "Instance Created by Terraform - Number 1"
+    }
+
+    connection {
+        user = "ubuntu"
+        key_file = "${var.key_path}"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+          "sudo service docker restart",
+          "docker run -d christhewintle/devops-class-node-app-with-config"
+        ]
+    }
+}
+
+
+
+
+
